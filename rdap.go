28ef074d4179618097f14557f1f16028255e3594
Mohamed Helal <mohamed@helal.me>
2024-12-14 19:44:40 +0200

    feat: add main package file
    - Add main package file for better Go package documentation
    - Define package version constant

---
+++ rdap.go
@@ -0,0 +1,6 @@
+// Package rdap provides a high-performance Registration Data Access Protocol (RDAP)
+// service and command-line tool for IP, ASN, and domain lookups.
+package rdap
+
+// Version is the current version of the RDAP package
+const Version = "0.1.0"
